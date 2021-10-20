b1a097b3e84857adc8d147a4310d455ed2fec20a
SungJin Kim <sungjinkimm@gmail.com>
2021-10-20 11:40:08 -0400

    initial heroku commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const colors = require('colors');
@@ -24,6 +25,14 @@ const PORT = process.env.PORT || 5000;
 const transactions = require('./routes/transactions');
 
 app.use('/api/v1/transactions', transactions);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 app.listen(
   PORT,
