e1c87ad0ab7601c09f379ece7638cc202ff3eaa0
SungJin Kim <sungjinkimm@gmail.com>
2021-10-20 11:57:12 -0400

    commit 3

--- server.js
+++ server.js
@@ -26,13 +26,13 @@ const transactions = require('./routes/transactions');
 
 app.use('/api/v1/transactions', transactions);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
-}
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//   );
+// }
 
 app.listen(
   PORT,
